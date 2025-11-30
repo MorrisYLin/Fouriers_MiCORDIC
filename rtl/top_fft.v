e05827238f62415b7d0a8d25953ca010ec7ea3fe
MorrisYLin <morrislinyuan@gmail.com>
2025-11-30 11:18:28 -0600

    put on laptop

--- rtl/top_fft.v
+++ rtl/top_fft.v
@@ -1,4 +1,4 @@
-module top (
+module top_fft (
     input clk_i,
     input valid_i,
     input [15:0] x_re_i [0:15],     // Time-domain, 16 samples
@@ -8,23 +8,23 @@ module top (
     // Twiddle factors
     // TW_RE[k] = cos(2πk/8), Q15, unsigned hex format
     localparam [15:0] TW_RE [0:3] = {
-        16'h7FFF,  // k = 0  ( +1.0000 )
-        16'h5A82,  // k = 1  ( +0.7071 )
-        16'h0000,  // k = 2  (  0.0000 )
-        16'hA57E   // k = 3  ( -0.7071 )
+        16'h7FFF,
+        16'h5A82,
+        16'h0000,
+        16'hA57E
     };
 
     // TW_IM[k] = -sin(2πk/8), Q15, unsigned hex format
     localparam [15:0] TW_IM [0:3] = {
-        16'h0000,  // k = 0  (  0.0000 )
-        16'hA57E,  // k = 1  ( -0.7071 )
-        16'h8000,  // k = 2  ( -1.0000 )
-        16'hA57E   // k = 3  ( -0.7071 )
+        16'h0000,
+        16'hA57E,
+        16'h8000,
+        16'hA57E
     };
 
     // Stage 0
     butterfly b0 (
 
-    )
+    );
 
 endmodule
