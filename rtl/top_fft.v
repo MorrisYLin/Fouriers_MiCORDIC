895dad98dd62f7e0be182185a13273f0eda63482
Morris Lin <morrislinyuan@gmail.com>
2025-12-01 17:28:14 -0600

    desktop

--- rtl/top_fft.v
+++ rtl/top_fft.v
@@ -37,8 +37,9 @@ module top_fft (
 
     // Stage 0 outputs
     wire [20:0] s0_re [0:7];
-    wire [20:0] a_im [0:7];
+    wire [20:0] s0_im [0:7];
 
+    // Stage 0 butterflies
     butterfly #(
         .DATA_WIDTH(21),
         .FRAC_BITS(15)
@@ -51,7 +52,7 @@ module top_fft (
         .b_re_i(x_re_signed[1]),
         .b_im_i(x_im_signed[1]),
         .a_re_o(s0_re[0]),
-        .a_im_o(a_im[0]),
+        .a_im_o(s0_im[0]),
         .b_re_o(s0_re[1]),
         .b_im_o(s0_re[1])
     );
@@ -68,7 +69,7 @@ module top_fft (
         .b_re_i(x_re_signed[3]),
         .b_im_i(x_im_signed[3]),
         .a_re_o(s0_re[2]),
-        .a_im_o(a_im[2]),
+        .a_im_o(s0_im[2]),
         .b_re_o(s0_re[3]),
         .b_im_o(s0_re[3])
     );
@@ -85,7 +86,7 @@ module top_fft (
         .b_re_i(x_re_signed[5]),
         .b_im_i(x_im_signed[5]),
         .a_re_o(s0_re[4]),
-        .a_im_o(a_im[4]),
+        .a_im_o(s0_im[4]),
         .b_re_o(s0_re[5]),
         .b_im_o(s0_re[5])
     );
@@ -102,30 +103,151 @@ module top_fft (
         .b_re_i(x_re_signed[7]),
         .b_im_i(x_im_signed[7]),
         .a_re_o(s0_re[6]),
-        .a_im_o(a_im[6]),
+        .a_im_o(s0_im[6]),
         .b_re_o(s0_re[7]),
         .b_im_o(s0_re[7])
     );
 
     // Stage 1 outputs
     wire [20:0] s1_re [0:7];
-    wire [20:0] s2_im [0:7];
+    wire [20:0] s1_im [0:7];
 
+    // Stage 1 butterflies
     butterfly #(
         .DATA_WIDTH(21),
         .FRAC_BITS(15)
     ) butter1_0 (
         // input clk_i,
-        .twid_re_i(TW_RE_0),
-        .twid_im_i(TW_IM_0),
-        .a_re_i(x_re_signed[0]),
-        .a_im_i(x_im_signed[0]),
-        .b_re_i(x_re_signed[1]),
-        .b_im_i(x_im_signed[1]),
-        .a_re_o(s0_re[0]),
-        .a_im_o(a_im[0]),
-        .b_re_o(s0_re[1]),
-        .b_im_o(s0_re[1])
+        .twid_re_i(),
+        .twid_im_i(),
+        .a_re_i(s0_re[0]),
+        .a_im_i(s0_im[0]),
+        .b_re_i(s0_re[2]),
+        .b_im_i(s0_im[2]),
+        .a_re_o(s1_re[0]),
+        .a_im_o(s1_im[0]),
+        .b_re_o(s1_re[2]),
+        .b_im_o(s1_im[2])
+    );
+
+    butterfly #(
+        .DATA_WIDTH(21),
+        .FRAC_BITS(15)
+    ) butter1_1 (
+        // input clk_i,
+        .twid_re_i(),
+        .twid_im_i(),
+        .a_re_i(s0_re[1]),
+        .a_im_i(s0_im[1]),
+        .b_re_i(s0_re[3]),
+        .b_im_i(s0_im[3]),
+        .a_re_o(s1_re[1]),
+        .a_im_o(s1_im[1]),
+        .b_re_o(s1_re[3]),
+        .b_im_o(s1_im[3])
+    );
+
+    butterfly #(
+        .DATA_WIDTH(21),
+        .FRAC_BITS(15)
+    ) butter1_2 (
+        // input clk_i,
+        .twid_re_i(),
+        .twid_im_i(),
+        .a_re_i(s0_re[4]),
+        .a_im_i(s0_im[4]),
+        .b_re_i(s0_re[6]),
+        .b_im_i(s0_im[6]),
+        .a_re_o(s1_re[4]),
+        .a_im_o(s1_im[4]),
+        .b_re_o(s1_re[6]),
+        .b_im_o(s1_im[6])
+    );
+
+    butterfly #(
+        .DATA_WIDTH(21),
+        .FRAC_BITS(15)
+    ) butter1_3 (
+        // input clk_i,
+        .twid_re_i(),
+        .twid_im_i(),
+        .a_re_i(s0_re[5]),
+        .a_im_i(s0_im[5]),
+        .b_re_i(s0_re[7]),
+        .b_im_i(s0_im[7]),
+        .a_re_o(s1_re[5]),
+        .a_im_o(s1_im[5]),
+        .b_re_o(s1_re[7]),
+        .b_im_o(s1_im[7])
+    );
+
+    // Stage 2 Butterflies
+    butterfly #(
+        .DATA_WIDTH(21),
+        .FRAC_BITS(15)
+    ) butter2_0 (
+        // input clk_i,
+        .twid_re_i(),
+        .twid_im_i(),
+        .a_re_i(s1_re[0]),
+        .a_im_i(s1_im[0]),
+        .b_re_i(s1_re[4]),
+        .b_im_i(s1_im[4]),
+        .a_re_o(y_re_o[0]),
+        .a_im_o(y_im_o[0]),
+        .b_re_o(y_re_o[4]),
+        .b_im_o(y_re_o[4])
+    );
+
+    butterfly #(
+        .DATA_WIDTH(21),
+        .FRAC_BITS(15)
+    ) butter2_1 (
+        // input clk_i,
+        .twid_re_i(),
+        .twid_im_i(),
+        .a_re_i(s1_re[1]),
+        .a_im_i(s1_im[1]),
+        .b_re_i(s1_re[5]),
+        .b_im_i(s1_im[5]),
+        .a_re_o(y_re_o[1]),
+        .a_im_o(y_im_o[1]),
+        .b_re_o(y_re_o[5]),
+        .b_im_o(y_re_o[5])
+    );
+
+    butterfly #(
+        .DATA_WIDTH(21),
+        .FRAC_BITS(15)
+    ) butter2_2 (
+        // input clk_i,
+        .twid_re_i(),
+        .twid_im_i(),
+        .a_re_i(s1_re[2]),
+        .a_im_i(s1_im[2]),
+        .b_re_i(s1_re[6]),
+        .b_im_i(s1_im[6]),
+        .a_re_o(y_re_o[2]),
+        .a_im_o(y_im_o[2]),
+        .b_re_o(y_re_o[6]),
+        .b_im_o(y_re_o[6])
+    );
+
+    butterfly #(
+        .DATA_WIDTH(21),
+        .FRAC_BITS(15)
+    ) butter2_3 (
+        // input clk_i,
+        .twid_re_i(),
+        .twid_im_i(),
+        .a_re_i(s1_re[3]),
+        .a_im_i(s1_im[3]),
+        .b_re_i(s1_re[7]),
+        .b_im_i(s1_im[7]),
+        .a_re_o(y_re_o[3]),
+        .a_im_o(y_im_o[3]),
+        .b_re_o(y_re_o[7]),
+        .b_im_o(y_re_o[7])
     );
 
 endmodule
